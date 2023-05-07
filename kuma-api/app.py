28bf4e55c6ae480c951f08aec95f6215904eba99
motailab <mdmotailab@gmail.com>
2023-05-07 11:54:08 +0600

    update kuma api according to instruction and tested it's working.

--- kuma-api/app.py
+++ kuma-api/app.py
@@ -1,35 +1,32 @@
 import os
-from fastapi import FastAPI, Request, Depends, HTTPException
-from uptime_kuma_api import UptimeKumaApi, MonitorType
+from typing import List, Optional
+
 from pydantic import BaseModel
+
+from fastapi import FastAPI, Request, HTTPException
+
+from uptime_kuma_api import UptimeKumaApi, MonitorType
 
 app = FastAPI()
 
 
-class UptimeKumaMiddleware:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.api = None
-        print("username ", username)
-        print("password ", password)
-
-    async def __call__(self, request: Request, call_next):
-        if not self.api:
-            self.api = UptimeKumaApi(os.getenv('UPTIME_KUMA_SERVER'))
-            self.api.login(self.username, self.password)
-        response = await call_next(request)
-        self.api.disconnect()
-        return response
-
-
 class AddMonitorModel(BaseModel):
     name: str
+    ips: Optional[List[str]] = []
+    ports: Optional[List[int]] = []
+    domains: Optional[List[str]] = []
 
 
 class PauseMonitorModel(BaseModel):
     http_monitor_id: int
     dns_monitor_id: int
+
+
+class DeleteMonitorModel(BaseModel):
+    ips: Optional[List[str]] = []
+    ports: Optional[List[int]] = []
+    monitor_ids: Optional[List[int]] = []
+    domains: Optional[List[str]] = []
 
 
 @app.post("/pause_monitor")
@@ -50,47 +47,89 @@ async def pause_monitor(data: PauseMonitorModel):
         return {"message": f"HTTP:{data.http_monitor_id}, DNS:{data.dns_monitor_id} Monitor paused successfully."}
     except Exception as e:
         print(e)
-        api.disconnect()
+        if api:
+            api.disconnect()
         raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post("/add_monitor")
 async def add_monitor(data: AddMonitorModel):
     """Add a monitor to Uptime Kuma."""
+    print("data ", data)
     api = None
     try:
         username = os.getenv('UPTIME_KUMA_USERNAME')
         password = os.getenv('UPTIME_KUMA_PASSWORD')
         api = UptimeKumaApi(os.getenv('UPTIME_KUMA_SERVER'))
         api.login(username, password)
-        print(data)
         name = data.name
-        print('name ', name)
-        http = api.add_monitor(
-            type=MonitorType.HTTP, name=f"{name} - HTTP", url=f"https://{name}.app.runonflux.io", hostname=f"{name}.app.runonflux.io")
-        dns = api.add_monitor(
-            type=MonitorType.DNS, name=f"{name} - DNS", url=f"https://{name}.app.runonflux.io", hostname=f"{name}.app.runonflux.io")
+        print("name adding ", name)
+        existing_monitors = api.get_monitors()
+
+        http_monitor_id, dns_monitor_id = None, None
+        if not (name.startswith("PresearchNode") or name.startswith("BrokerNode")):
+            if f"{name} - HTTP" not in (monitor['name'] for monitor in existing_monitors):
+                http_monitor_id = api.add_monitor(
+                    type=MonitorType.HTTP, name=f"{name} - HTTP", url=f"https://{name}.app.runonflux.io", hostname=f"{name}.app.runonflux.io")['monitorID']
+            if f"{name} - DNS" not in (monitor['name'] for monitor in existing_monitors):
+                dns_monitor_id = api.add_monitor(
+                    type=MonitorType.DNS, name=f"{name} - DNS", url=f"https://{name}.app.runonflux.io", hostname=f"{name}.app.runonflux.io")['monitorID']
+
+        port_monitor_ids = []
+        if data.ips and data.ports:
+            for port in data.ports:
+                for ip in data.ips:
+                    port_monitor = api.add_monitor(
+                        type=MonitorType.PORT, name=f"{name} - {ip}:{port}", hostname=ip, port=port)
+                    port_monitor_ids.append(port_monitor['monitorID'])
+
+        if data.domains:
+            for domain in data.domains:
+                api.add_monitor(
+                    type=MonitorType.HTTP, name=f"{name} - HTTP ({domain})", url=f"https://{domain}", hostname=domain)
+                api.add_monitor(
+                    type=MonitorType.DNS, name=f"{name} - DNS ({domain})", hostname=domain)
+
         api.disconnect()
-        return {"message": f"successfully added monitor for {name}", "http_monitor_id": http['monitorID'], "dns_monitor_id": dns['monitorID']}
+        return {"message": f"successfully added monitor for {name}",
+                "http_monitor_id": http_monitor_id,
+                "dns_monitor_id": dns_monitor_id,
+                "port_monitor_ids": port_monitor_ids}
     except Exception as e:
         print(e)
-        api.disconnect()
+        if api:
+            api.disconnect()
         raise HTTPException(status_code=500, detail=str(e))
 
 
-# @app.middleware("http")
-# async def add_api_client(request: Request, call_next):
-#     try:
-#         username = os.getenv('UPTIME_KUMA_USERNAME')
-#         password = os.getenv('UPTIME_KUMA_PASSWORD')
-#         print("username2 ", username)
-#         print("password2 ", password)
-#         request.state.api = UptimeKumaApi(os.getenv('UPTIME_KUMA_SERVER'))
-#         request.state.api.login(username, password)
-#         response = await call_next(request)
-#         request.state.api.disconnect()
-#         return response
-#     except Exception as e:
-#         print(e)
-#         response = await call_next(request)
-#         return response
+@app.post("/delete_monitors")
+async def delete_monitors(data: DeleteMonitorModel):
+    """Delete monitors in Uptime Kuma."""
+    api = None
+    try:
+        username = os.getenv('UPTIME_KUMA_USERNAME')
+        password = os.getenv('UPTIME_KUMA_PASSWORD')
+        api = UptimeKumaApi(os.getenv('UPTIME_KUMA_SERVER'))
+        api.login(username, password)
+        monitors = api.get_monitors()
+        deleted_monitors = []
+
+        for monitor in monitors:
+            if (data.monitor_ids and monitor['id'] in data.monitor_ids) or \
+               (monitor['type'] == 'port' and ((data.ips or data.ports) and (monitor['hostname'] in data.ips or monitor['port'] in data.ports))) or \
+               (monitor['type'] in ['http', 'dns'] and data.domains and any(domain in monitor['url'] for domain in data.domains)):
+                api.delete_monitor(monitor['id'])
+                deleted_monitors.append(monitor['id'])
+
+            # if (data.monitor_ids and monitor['id'] in data.monitor_ids) or \
+            #         (monitor['type'] == 'port' and ((data.ips or data.ports and (monitor['hostname'] in data.ips) or (monitor['port'] in data.ports)))):
+            #     api.delete_monitor(monitor['id'])
+            #     deleted_monitors.append(monitor['id'])
+
+        api.disconnect()
+        return {"message": f"Monitors deleted successfully.", "deleted_monitors": deleted_monitors}
+    except Exception as e:
+        print(e)
+        if api:
+            api.disconnect()
+        raise HTTPException(status_code=500, detail=str(e))
--- scheduler/main.js
+++ scheduler/main.js
@@ -6,9 +6,11 @@ const schedule = require("node-schedule");
 const recordSchema = new mongoose.Schema({
   name: String,
   ports: [Number],
+  ips: [String],
   height: Number,
   dns_monitor_id: Number,
   http_monitor_id: Number,
+  domains: [String],
 });
 
 const api = axios.create({
@@ -41,8 +43,18 @@ async function checkRecords() {
       const record = data[i];
       const expire = record?.expire;
       const height = record?.height;
-      const ports = record.compose?.map((item) => item.ports)?.flat();
+      const ports = record.compose
+        ?.map((item) => item.ports)
+        ?.flat()
+        ?.map(Number);
+      const domains = (
+        record?.domains ??
+        record.compose?.map((item) => item.domains)?.flat() ??
+        []
+      ).filter((domain) => domain.trim());
+
       if (!expire) continue;
+      const ipList = await getIPList(record.name);
 
       if (expire + height > blocks && ports?.length) {
         // Check if the record exists in MongoDB
@@ -52,27 +64,141 @@ async function checkRecords() {
           // Create a new Monitor in MongoDB and call another API with the name as the request body
           let monitorAdded = null;
           try {
-            monitorAdded = await api.post("/add_monitor", {
+            const payload = {
               name: record.name,
-            });
+              ips: ipList,
+              ports: ports,
+            };
+            // console.log("sedning  ", payload);
+            monitorAdded = await api.post("/add_monitor", payload);
+            // console.log("monitorAdded ", monitorAdded);
           } catch (error) {
+            console.log("error ", error?.message);
             console.log("unable to add monitor ", record.name);
+            // break;
             continue;
           }
           const newRecord = new Monitor({
             name: record.name,
             ports: ports,
             height: height,
+            ips: ipList,
             dns_monitor_id: monitorAdded.data.dns_monitor_id,
             http_monitor_id: monitorAdded.data.http_monitor_id,
+            domains,
           });
           await newRecord.save();
           console.log("record added");
-        } else if (height > existingRecord.height) {
-          // Update the height of the existing record in MongoDB
-          existingRecord.height = height;
-          await existingRecord.save();
-          console.log("record updated");
+        }
+        // else if (height > existingRecord.height) {
+        //   // Update the height of the existing record in MongoDB
+        //   existingRecord.height = height;
+        //   await existingRecord.save();
+        //   console.log("record updated");
+        // }
+        else {
+          const updatedIps = ipList.filter(
+            (ip) => !existingRecord.ips.includes(ip)
+          );
+          const updatedPorts = ports.filter(
+            (port) => !existingRecord.ports.includes(port)
+          );
+
+          const updatedDomains = domains.filter(
+            (domain) => !existingRecord.domains.includes(domain)
+          );
+
+          const deletedIps = existingRecord.ips.filter(
+            (ip) => !ipList.includes(ip)
+          );
+          const deletedPorts = existingRecord.ports.filter(
+            (port) => !ports.includes(port)
+          );
+          const deletedDomains = existingRecord.domains.filter(
+            (domain) => !domains.includes(domain)
+          );
+
+          if (
+            deletedIps.length > 0 ||
+            deletedPorts.length > 0 ||
+            deletedDomains.length > 0
+          ) {
+            try {
+              await api.post("/delete_monitors", {
+                ips: deletedIps,
+                ports: deletedPorts,
+                domains: deletedDomains,
+              });
+              console.log("monitor removed for changed ips ", deletedIps);
+              console.log("monitor removed for changed ports ", deletedPorts);
+              console.log(
+                "monitor removed for changed domains ",
+                deletedDomains
+              );
+            } catch (error) {
+              console.log(
+                "unable to delete monitors with non-matching IPs and ports: ",
+                error.message
+              );
+            }
+          }
+
+          if (updatedIps.length) {
+            try {
+              await api.post("/add_monitor", {
+                name: record.name,
+                ips: updatedIps,
+                ports: ports,
+              });
+            } catch (error) {
+              console.log(
+                `unable to add new monitors for latest ip `,
+                error?.message
+              );
+            }
+          }
+
+          if (updatedPorts.length) {
+            try {
+              await api.post("/add_monitor", {
+                name: record.name,
+                ips: ipList,
+                ports: updatedPorts,
+              });
+            } catch (error) {
+              console.log(
+                `unable to add new monitors for latest ip `,
+                error?.message
+              );
+            }
+          }
+
+          if (updatedDomains.length) {
+            try {
+              await api.post("/add_monitor", {
+                domains: updatedDomains,
+              });
+            } catch (error) {
+              console.log(
+                `unable to add new monitors for latest ip `,
+                error?.message
+              );
+            }
+          }
+
+          if (
+            height > existingRecord.height ||
+            updatedIps.length > 0 ||
+            updatedPorts.length > 0
+          ) {
+            // Update the height, IPs, and ports of the existing record in MongoDB
+            existingRecord.height = height;
+            existingRecord.ips = ipList;
+            existingRecord.ports = ports;
+            existingRecord.domains = domains;
+            await existingRecord.save();
+            console.log("record updated");
+          }
         }
       } else {
         // If the record is not expired, check if it exists in MongoDB and delete it if it does
@@ -87,6 +213,12 @@ async function checkRecords() {
               "monitor paused and record removing ",
               existingRecord.name
             );
+            // Delete all port monitors related to the existing record
+            // await api.post("/delete_monitors", {
+            //   ips: existingRecord.ips,
+            //   ports: existingRecord.ports,
+            //   domains: existingRecord.domains,
+            // });
             await existingRecord.remove();
           } catch (error) {
             console.log("unable to pause or remove record ", error?.message);
@@ -100,6 +232,26 @@ async function checkRecords() {
   }
 }
 
+async function getIPList(name) {
+  const resp = await axios.get(
+    `https://api.runonflux.io/apps/location/${name}`
+  );
+  const appIPs = resp.data.data;
+  return appIPs.map((app) => app.ip.split(":")[0].trim()).filter((ip) => ip);
+}
+
+async function startMonitoringLoop() {
+  while (true) {
+    try {
+      await checkRecords();
+      console.log("Waiting for restart...");
+      await new Promise((resolve) => setTimeout(resolve, 3 * 60 * 1000)); // Wait for 3 minutes
+    } catch (error) {
+      console.error("Error in monitoring loop:", error?.message);
+    }
+  }
+}
+
 // Connect to MongoDB
 mongoose
   .connect(mongoUrl, {
@@ -108,9 +260,7 @@ mongoose
   })
   .then(async () => {
     console.log("Connected to MongoDB");
-    await checkRecords();
-    // Schedule the checkRecords function to run every 10 minutes
-    const job = schedule.scheduleJob("*/10 * * * *", checkRecords);
+    startMonitoringLoop();
   })
   .catch((error) => {
     console.error(error?.message);
